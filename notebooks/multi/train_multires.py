bc26d949217effdbbbc7a5f34c76fc0c22220d1e
loc <xloctran@gmail.com>
2020-04-15 15:38:13 +0200

    add training file

---
+++ notebooks/multi/train_multires.py
@@ -0,0 +1,307 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+dataname="trg-multi-7classes-10x"
+class_names=["Fat", "Muscular", "Vein", "Gland", "Stroma", "Tumor", "Epithelium"]
+nclasses=len(class_names)
+in_channels= 3  #input channel of the data, RGB = 3
+
+
+
+# --- training params
+batch_size=256
+patch_size=64 #currently, this needs to be 224 due to densenet architecture
+num_epochs = 100
+phases = ["train","val"] #how many phases did we create databases for?
+#validation_phases= ["val"] #when should we do valiation? note that validation is *very* time consuming, so as opposed to doing for both training and validation, we do it only for vlaidation at the end of the epoch
+                           #additionally, using simply [], will skip validation entirely, drastically speeding things up
+validation_phases= [] #when should we do valiation? note that validation is *very* time consuming, so as opposed to doing for both training and validation, we do it only for vlaidation at the end of the epoch
+                           #additionally, using simply [], will skip validation entirely, drastically speeding things up    
+
+# +
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.models import DenseNet
+from torchsummary import summary
+
+from albumentations import *
+from albumentations.pytorch import ToTensor
+
+import PIL
+import matplotlib.pyplot as plt
+import cv2
+
+import scipy
+import numpy as np
+import sys, glob
+
+from tensorboardX import SummaryWriter
+
+from tqdm.autonotebook import tqdm
+
+
+import time
+import math
+import tables
+
+import random
+
+from sklearn.metrics import confusion_matrix
+
+
+# -
+
+#helper function for pretty printing of current time and remaining time
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent+.00001)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
+gpuid=0
+#specify if we should use a GPU (cuda) or only the CPU
+print(torch.cuda.get_device_properties(gpuid))
+torch.cuda.set_device(gpuid)
+device = torch.device(f'cuda:{gpuid}' if torch.cuda.is_available() else 'cpu')
+
+checkpoint = torch.load("./trg_multi_multires_latest_model.pth", map_location=device)
+checkpointbest = torch.load("./trg_multi_multires_best_v1.pth", map_location=device)
+
+#from torchvision.models import DenseNet
+import sys
+sys.path.insert(0,'/home/ltran/trg_ltran/')
+from src.models.MultiResSmallNetwork import MergeSmallNetworks
+
+model=MergeSmallNetworks(nclasses = nclasses, outputsize = 7 ).to(device)
+#model.load_state_dict(checkpoint["model_dict"])
+
+checkpointbest['best_loss_on_test']['val']
+
+summary(model.res1,input_size=(3,64,64))
+
+
+# +
+#summary(model.res1.encoder,input_size=(3,64,64))
+# -
+
+#this defines our dataset class which will be used by the dataloader
+class Dataset(object):
+    def __init__(self, fname ,img_transform=None):
+        #nothing special here, just internalizing the constructor parameters
+        self.fname=fname
+
+        self.img_transform=img_transform
+        
+        with tables.open_file(self.fname,'r') as db:
+            self.classsizes=db.root.classsizes[:]
+            self.nitems=db.root.imgres1.shape[0]
+        
+        
+    def __getitem__(self, index):
+        #opening should be done in __init__ but seems to be
+        #an issue with multithreading so doing here. need to do it everytime, otherwise hdf5 crashes
+
+        with tables.open_file(self.fname,'r') as db:
+            self.imgres1=db.root.imgres1
+            self.imgres2=db.root.imgres2
+            
+            self.labels=db.root.labels
+
+            #get the requested image and mask from the pytable
+            imgres1 = self.imgres1[index,:,:,:]
+            imgres2 = self.imgres2[index,:,:,:]
+            
+            label = self.labels[index]
+        
+        
+        imgres1_new = imgres1
+        imgres2_new = imgres2
+        
+        if self.img_transform:
+            #img_new = self.img_transform(image=img)['image']
+            trans = self.img_transform(image=imgres1,imgres2=imgres2)
+            imgres1_new = trans['image']
+            imgres2_new = trans['imgres2']
+
+        return imgres1_new, imgres2_new, label
+    def __len__(self):
+        return self.nitems
+
+#https://github.com/albu/albumentations/blob/master/notebooks/migrating_from_torchvision_to_albumentations.ipynb
+img_transform = Compose([
+        RandomScale(scale_limit=0.1,p=.9),
+        PadIfNeeded(min_height=patch_size,min_width=patch_size),        
+        VerticalFlip(p=.5),
+        HorizontalFlip(p=.5),
+        #Blur(p=.5),
+        #Downscale(p=.25, scale_min=0.64, scale_max=0.99),
+        GaussNoise(p=.5, var_limit=(10.0, 50.0)),
+        GridDistortion(p=.5, num_steps=5, distort_limit=(-0.3, 0.3), 
+                       border_mode=cv2.BORDER_REFLECT),
+        ISONoise(p=.5, intensity=(0.1, 0.5), color_shift=(0.01, 0.05)),
+        RandomBrightness(p=.5, limit=(-0.2, 0.2)),
+        RandomContrast(p=.5, limit=(-0.2, 0.2)),
+        RandomGamma(p=.5, gamma_limit=(80, 120), eps=1e-07),
+        MultiplicativeNoise(p=.5, multiplier=(0.9, 1.1), per_channel=True, elementwise=True),
+        HueSaturationValue(hue_shift_limit=20,sat_shift_limit=10,val_shift_limit=10,p=.9),
+        Rotate(p=1, border_mode=cv2.BORDER_REFLECT),
+        CenterCrop(patch_size,patch_size),
+        ToTensor()
+    ], additional_targets={'imgres2':'image'})
+
+dataset={}
+dataLoader={}
+for phase in phases: #now for each of the phases, we're creating the dataloader
+                     #interestingly, given the batch size, i've not seen any improvements from using a num_workers>0
+    
+    dataset[phase]=Dataset(f"../../../data/processed/{dataname}-{phase}.pytable", img_transform=img_transform)
+    dataLoader[phase]=DataLoader(dataset[phase], batch_size=batch_size, 
+                                shuffle=True, num_workers=0,pin_memory=True) 
+    print(f"{phase} dataset size:\t{len(dataset[phase])}")
+
+# +
+#visualize a single example to verify that it is correct
+(imgres1,imgres2, label)=dataset["train"][0]
+fig, ax = plt.subplots(1,2, figsize=(10,4))  # 1 row, 2 columns
+
+#build output showing patch after augmentation and original patch
+ax[0].imshow(np.moveaxis(imgres1.numpy(),0,-1))
+ax[1].imshow(np.moveaxis(imgres2.numpy(),0,-1))
+
+
+# ax[0].imshow(imgres1)
+# ax[1].imshow(imgres2)
+
+
+print(class_names[label])
+# -
+
+optim = torch.optim.Adam(model.parameters(), weight_decay=.001) 
+
+# +
+#we have the ability to weight individual classes, in this case we'll do so based on their presense in the trainingset
+#to avoid biasing any particular class
+
+class_weight=dataset["train"].classsizes
+print(class_weight)
+#class_weight = torch.from_numpy(1-class_weight/class_weight.sum()).type('torch.FloatTensor').to(device)
+class_weight = torch.from_numpy(class_weight.max()/class_weight).type('torch.FloatTensor').to(device)
+
+print(class_weight) #show final used weights, make sure that they're reasonable before continouing
+criterion = nn.CrossEntropyLoss(weight = class_weight) 
+# -
+
+#def trainnetwork():
+writer=SummaryWriter() #open the tensorboard visualiser
+best_loss_on_test = np.Infinity
+start_time = time.time()
+for epoch in range(num_epochs):
+    #zero out epoch based performance variables 
+    all_acc = {key: 0 for key in phases} 
+    all_loss = {key: torch.zeros(0).to(device) for key in phases} #keep this on GPU for greatly improved performance
+    cmatrix = {key: np.zeros((nclasses,nclasses)) for key in phases}
+
+    for phase in phases: #iterate through both training and validation states
+
+        if phase == 'train':
+            model.train()  # Set model to training mode
+        else: #when in eval mode, we don't want parameters to be updated
+            model.eval()   # Set model to evaluate mode
+
+        for ii , (Xres1, Xres2, label) in tqdm(enumerate(dataLoader[phase]),total=len(dataLoader[phase])): #for each of the batches
+            Xres1 = Xres1.to(device)  # [Nbatch, 3, H, W]
+            Xres2 = Xres2.to(device)  # [Nbatch, 3, H, W]
+            label = label.type('torch.LongTensor').to(device)  # [Nbatch, 1] with class indices (0, 1, 2,...num_classes)
+
+            with torch.set_grad_enabled(phase == 'train'): #dynamically set gradient computation, in case of validation, this isn't needed
+                                                            #disabling is good practice and improves inference time
+
+                
+                prediction = model.dualfoward(Xres1,Xres2)  # [N, Nclass]
+                #prediction = model.res1(Xres1)  # [N, Nclass]
+                #prediction = model.res2(Xres2)  # [N, Nclass]
+                loss = criterion(prediction, label)
+
+
+                if phase=="train": #in case we're in train mode, need to do back propogation
+                    optim.zero_grad()
+                    loss.backward()
+                    optim.step()
+                    train_loss = loss
+
+
+                all_loss[phase]=torch.cat((all_loss[phase],loss.detach().view(1,-1)))
+
+                if phase in validation_phases: #if this phase is part of validation, compute confusion matrix
+                    p=prediction.detach().cpu().numpy()
+                    cpredflat=np.argmax(p,axis=1).flatten()
+                    yflat=label.cpu().numpy().flatten()
+
+                    CM = scipy.sparse.coo_matrix((np.ones(yflat.shape[0], dtype=np.int64), (yflat, cpredflat)),
+                            shape=(nclasses, nclasses), dtype=np.int64,
+                            ).toarray()
+                    cmatrix[phase]=cmatrix[phase]+CM
+                    
+
+        all_acc[phase]=(cmatrix[phase]/cmatrix[phase].sum()).trace()
+        all_loss[phase] = all_loss[phase].cpu().numpy().mean()
+
+        #save metrics to tensorboard
+        writer.add_scalar(f'{phase}/loss', all_loss[phase], epoch)
+        if phase in validation_phases:
+            writer.add_scalar(f'{phase}/acc', all_acc[phase], epoch)
+            for r in range(nclasses):
+                for c in range(nclasses): #essentially write out confusion matrix
+                    writer.add_scalar(f'{phase}/{r}{c}', cmatrix[phase][r][c],epoch)
+
+    print('%s ([%d/%d] %d%%), train loss: %.4f test loss: %.4f' % (timeSince(start_time, (epoch+1) / num_epochs), 
+                                                 epoch+1, num_epochs ,(epoch+1) / num_epochs * 100, all_loss["train"], all_loss["val"]),end="")    
+
+    #if current loss is the best we've seen, save model state with all variables
+    #necessary for recreation
+    if epoch in [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]:
+        state = {'epoch': epoch + 1,
+         'model_dict': model.state_dict(),
+         'optim_dict': optim.state_dict(),
+         'best_loss_on_test': all_loss,
+         'num_classes': nclasses}
+        torch.save(state, f"{dataname}-best.pth")
+    if all_loss["val"] < best_loss_on_test:
+        best_loss_on_test = all_loss["val"]
+        print("  **")
+        state = {'epoch': epoch + 1,
+         'model_dict': model.state_dict(),
+         'optim_dict': optim.state_dict(),
+         'best_loss_on_test': all_loss,
+         'num_classes': nclasses}
+        torch.save(state, f"{dataname}-best.pth")
+        torch.save(state, f"{dataname}-latest.pth")
+            
+    else:
+        print("")
+
+cmatrix
+
+Xres2
+
+
